760c30b5c79f401f0e4fa41f7262c4e794f5081e
Anassaitouaguerd <aitouaguerdanass@gmail.com>
2024-11-20 09:30:22 +0100

    repositry for arbre

---
+++ Citronix/src/main/java/com/example/Citronix/repository/arbre/ArbreRepository.java
@@ -0,0 +1,8 @@
+package com.example.Citronix.repository.arbre;
+
+import com.example.Citronix.entity.Arbre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArbreRepository extends JpaRepository<Arbre, Long> {
+}
+
