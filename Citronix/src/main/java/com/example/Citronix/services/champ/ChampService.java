8170e63424466d37e669c3648b5ce50d8b7e6a7a
Anassaitouaguerd <aitouaguerdanass@gmail.com>
2024-11-21 11:25:45 +0100

    fix in create methode for get the farme object

--- Citronix/src/main/java/com/example/Citronix/services/champ/ChampService.java
+++ Citronix/src/main/java/com/example/Citronix/services/champ/ChampService.java
@@ -3,26 +3,36 @@ package com.example.Citronix.services.champ;
 import com.example.Citronix.DTO.champ.ChampDTO;
 import com.example.Citronix.DTO.champ.ChampReqDTO;
 import com.example.Citronix.entity.Champ;
+import com.example.Citronix.entity.Ferme;
 import com.example.Citronix.mapper.ChampMapper;
 import com.example.Citronix.repository.champ.ChampRepository;
+import com.example.Citronix.repository.ferme.FermeRepository;
+import com.example.Citronix.services.ferme.FermeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ChampService {
     private final ChampRepository champRepository;
+    private final FermeRepository fermeRepository;
     private final ChampMapper champMapper;
 
     @Transactional
     public ChampDTO createChamp(ChampReqDTO reqDTO) {
-        return champMapper.toDTO(
-                champRepository.save(champMapper.toEntity(reqDTO)
-                ));
+
+        Optional<Ferme> ferme = Optional.ofNullable(fermeRepository.findById(reqDTO.getFerme_id())
+                .orElseThrow(() -> new RuntimeException("Ferme not found")));
+
+        Champ champ = champMapper.toEntity(reqDTO);
+        champ.setFerme(ferme.get());
+
+        return champMapper.toDTO(champRepository.save(champ));
     }
 
     @Transactional
@@ -30,8 +40,12 @@ public class ChampService {
         Champ existingChamp = champRepository.findById(id)
                                      .orElseThrow(() -> new RuntimeException("Champ not found"));
 
+        Optional<Ferme> ferme = Optional.ofNullable(fermeRepository.findById(reqDTO.getFerme_id())
+                .orElseThrow(() -> new RuntimeException("Ferme not found")));
+
         Champ champ = champMapper.toEntity(reqDTO);
         champ.setId(existingChamp.getId());
+        champ.setFerme(ferme.get());
         return champMapper.toDTO(champRepository.save(champ));
     }
 
