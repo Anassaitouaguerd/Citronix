52b9a10370df1f5e19b698243419e42662bc3293
Anassaitouaguerd <aitouaguerdanass@gmail.com>
2024-11-23 14:23:46 +0100

    update just the date of complition

--- target/generated-sources/annotations/com/example/Citronix/mapper/RecolteMapperImpl.java
+++ target/generated-sources/annotations/com/example/Citronix/mapper/RecolteMapperImpl.java
@@ -2,14 +2,11 @@ package com.example.Citronix.mapper;
 
 import com.example.Citronix.DTO.recolte.RecolteDTO;
 import com.example.Citronix.DTO.recolte.RecolteDetailDTO;
-import com.example.Citronix.DTO.vente.VenteDTO;
+import com.example.Citronix.DTO.recolte.RecolteReqDTO;
 import com.example.Citronix.entity.Arbre;
 import com.example.Citronix.entity.Champ;
 import com.example.Citronix.entity.Recolte;
 import com.example.Citronix.entity.RecolteDetails;
-import com.example.Citronix.entity.Vente;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.processing.Generated;
@@ -17,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-11-22T09:42:32+0100",
+    date = "2024-11-23T14:20:52+0100",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.2 (Oracle Corporation)"
 )
 @Component
@@ -36,26 +33,23 @@ public class RecolteMapperImpl implements RecolteMapper {
         recolteDTO.setSeason( harvest.getSeason() );
         recolteDTO.setTotalQuantity( harvest.getTotalQuantity() );
         recolteDTO.setRecolteDate( harvest.getRecolteDate() );
-        recolteDTO.setVents( venteListToVenteDTOList( harvest.getVents() ) );
+        recolteDTO.setRecolteDetails( recolteDetailsListToRecolteDetailDTOList( harvest.getRecolteDetails() ) );
 
         return recolteDTO;
     }
 
     @Override
-    public Recolte toEntity(RecolteDTO dto) {
+    public Recolte toEntity(RecolteReqDTO dto) {
         if ( dto == null ) {
             return null;
         }
 
-        Recolte recolte = new Recolte();
+        Recolte.RecolteBuilder recolte = Recolte.builder();
 
-        recolte.setId( dto.getId() );
-        recolte.setSeason( dto.getSeason() );
-        recolte.setRecolteDate( dto.getRecolteDate() );
-        recolte.setTotalQuantity( dto.getTotalQuantity() );
-        recolte.setVents( venteDTOListToVenteList( dto.getVents() ) );
+        recolte.champ( mapChamp( dto.getChampId() ) );
+        recolte.recolteDate( dto.getRecolteDate() );
 
-        return recolte;
+        return recolte.build();
     }
 
     @Override
@@ -79,12 +73,13 @@ public class RecolteMapperImpl implements RecolteMapper {
             return null;
         }
 
-        RecolteDetails recolteDetails = new RecolteDetails();
+        RecolteDetails.RecolteDetailsBuilder recolteDetails = RecolteDetails.builder();
 
-        recolteDetails.setId( dto.getId() );
-        recolteDetails.setQuantity( dto.getQuantity() );
+        recolteDetails.arbre( mapArbre( dto.getArbreId() ) );
+        recolteDetails.id( dto.getId() );
+        recolteDetails.quantity( dto.getQuantity() );
 
-        return recolteDetails;
+        return recolteDetails.build();
     }
 
     private Long harvestChampId(Recolte recolte) {
@@ -102,67 +97,14 @@ public class RecolteMapperImpl implements RecolteMapper {
         return id;
     }
 
-    protected VenteDTO venteToVenteDTO(Vente vente) {
-        if ( vente == null ) {
-            return null;
-        }
-
-        VenteDTO venteDTO = new VenteDTO();
-
-        venteDTO.setId( vente.getId() );
-        if ( vente.getDate() != null ) {
-            venteDTO.setDate( DateTimeFormatter.ISO_LOCAL_DATE.format( vente.getDate() ) );
-        }
-        venteDTO.setClient( vente.getClient() );
-        if ( vente.getRevenue() != null ) {
-            venteDTO.setRevenue( String.valueOf( vente.getRevenue() ) );
-        }
-        venteDTO.setRecolte( vente.getRecolte() );
-
-        return venteDTO;
-    }
-
-    protected List<VenteDTO> venteListToVenteDTOList(List<Vente> list) {
+    protected List<RecolteDetailDTO> recolteDetailsListToRecolteDetailDTOList(List<RecolteDetails> list) {
         if ( list == null ) {
             return null;
         }
 
-        List<VenteDTO> list1 = new ArrayList<VenteDTO>( list.size() );
-        for ( Vente vente : list ) {
-            list1.add( venteToVenteDTO( vente ) );
-        }
-
-        return list1;
-    }
-
-    protected Vente venteDTOToVente(VenteDTO venteDTO) {
-        if ( venteDTO == null ) {
-            return null;
-        }
-
-        Vente vente = new Vente();
-
-        vente.setId( venteDTO.getId() );
-        if ( venteDTO.getDate() != null ) {
-            vente.setDate( LocalDate.parse( venteDTO.getDate() ) );
-        }
-        vente.setClient( venteDTO.getClient() );
-        if ( venteDTO.getRevenue() != null ) {
-            vente.setRevenue( Double.parseDouble( venteDTO.getRevenue() ) );
-        }
-        vente.setRecolte( venteDTO.getRecolte() );
-
-        return vente;
-    }
-
-    protected List<Vente> venteDTOListToVenteList(List<VenteDTO> list) {
-        if ( list == null ) {
-            return null;
-        }
-
-        List<Vente> list1 = new ArrayList<Vente>( list.size() );
-        for ( VenteDTO venteDTO : list ) {
-            list1.add( venteDTOToVente( venteDTO ) );
+        List<RecolteDetailDTO> list1 = new ArrayList<RecolteDetailDTO>( list.size() );
+        for ( RecolteDetails recolteDetails : list ) {
+            list1.add( toDetailDTO( recolteDetails ) );
         }
 
         return list1;
