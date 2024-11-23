cf1a415df6b3f2f2f863cee1d6ca9dc614e2de12
Anassaitouaguerd <aitouaguerdanass@gmail.com>
2024-11-23 15:01:07 +0100

    complete the operation of crud with saving vent with calcule revune of the vente to this recolte  and apply all validations

--- Citronix/src/main/java/com/example/Citronix/entity/Champ.java
+++ Citronix/src/main/java/com/example/Citronix/entity/Champ.java
@@ -1,7 +1,9 @@
 package com.example.Citronix.entity;
 
 import jakarta.persistence.*;
+import jdk.jshell.Snippet;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +14,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Champ {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
